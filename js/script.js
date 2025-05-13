75b92b23665ba519152b7414971f3fc66edbe986
bhavesh chawke <chandachawke754@gmail.com>
2025-05-13 18:42:40 +0530

    third commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,45 @@
-let loginBtn = document.querySelector("button");
-let message = document.querySelector(".loginBox p");
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-loginBtn.addEventListener("click", () => {
-  if (email.value === "" && password.value === "") {
-    message.innerHTML = "Abe pahle email aur password to bhar le😁";
-  } else {
-    message.innerHTML = "";
-  }
+// let loginBtn = document.querySelector("button");
+// let message = document.querySelector(".loginBox p");
+// let email = document.getElementById("email");
+// let password = document.getElementById("password");
+// loginBtn.addEventListener("click", () => {
+//   if (email.value === "" && password.value === "") {
+//     message.innerHTML = "Abe pahle email aur password to bhar le😁";
+//   } else {
+//     message.innerHTML = "";
+//   }
+// });
+
+// document.getElementById("signUpForm").addEventListener('submit',(e)=>{
+// e.preventDefault();
+//   let sName = document.getElementById("sName").value;
+//   let sEmail = document.getElementById("sEmail").value;
+//   let sPassword = document.getElementById("sPassword").value;
+//   localStorage.setItem("S_username", sName);
+//   localStorage.setItem("S_Email", sEmail);
+//   localStorage.setItem("S_Password", sPassword);
+
+// })
+// let button=document.getElementById("sBtn");
+// button.addEventListener('click',(e)=>{
+//   e.preventDefault();
+//   window.onload()
+// })
+
+//rsponsive section
+let slider= document.getElementById("slider");
+let sliderSection=document.querySelector('.slideSection');
+let sliderSection2=document.querySelector('.slideSection2');
+slider.addEventListener('click',()=>{
+if(sliderSection.style.display==='none' || sliderSection.style.display==='' && sliderSection2.style.display==='none' || sliderSection2.style.display===''){
+  sliderSection.style.display='flex';
+  sliderSection2.style.display='flex';
+}else{
+  sliderSection.style.display='none';
+  sliderSection2.style.display='none';
+
+}
 });
-
-document.getElementById("signUpForm").addEventListener('submit',(e)=>{
-e.preventDefault();
-  let sName = document.getElementById("sName").value;
-  let sEmail = document.getElementById("sEmail").value;
-  let sPassword = document.getElementById("sPassword").value;
-  localStorage.setItem("S_username", sName);
-  localStorage.setItem("S_Email", sEmail);
-  localStorage.setItem("S_Password", sPassword);
-
-})
-let button=document.getElementById("sBtn");
-button.addEventListener('click',(e)=>{
-  e.preventDefault();
-  window.onload()
-})
 
   
 
